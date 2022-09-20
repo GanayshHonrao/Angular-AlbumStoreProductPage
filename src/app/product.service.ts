46af5488f447cb4c666bef287f966721bbe674c3
GanayshHonrao <ganaysh.honrao@gmail.com>
2022-09-20 14:51:20 +0530

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
+import {Observable} from 'rxjs';
 import "rxjs/add/operator/map";
+import { from } from 'rxjs/observable/from';
 
 @Injectable()
 export class ProductService {
